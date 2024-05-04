ace34edf75610a3e3119659c4dba6b6a096211de
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-05-04 14:30:05 +0300

    feat: config using .env file

--- backend/overseer/main.py
+++ backend/overseer/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
+from overseer.config import base as config
+
 
 app = FastAPI()
 
 
 @app.get("/")
 def home():
-    return {"Hello": "World"}
+    return {
+        "Hello": "World",
+        "SECRET_KEY": config.SECRET_KEY,
+    }
