40df6ab1cb836bf579f150c1a2b7e53953e872ff
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-05-28 13:25:39 +0300

    feat: update user profile with patch

--- backend/overseer/db/users.py
+++ backend/overseer/db/users.py
@@ -1,19 +1,19 @@
 import uuid
 from datetime import datetime
-from typing import Optional
+from typing import override
 
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
 from pymongo import IndexModel
 from beanie import Document
-from pydantic import BaseModel, EmailStr, Field, SecretStr
+from pydantic import BaseModel, EmailStr, SecretStr, Field
 from starlette.authentication import BaseUser
 
 
 class User(Document, BaseUser):
     username: str = Field(min_length=4, max_length=128)
     password: SecretStr = Field(min_length=8, max_length=128)
-    email: Optional[EmailStr] = None
+    email: EmailStr | None = None
     is_active: bool = Field(default=True)
     created_at: datetime = datetime.now()
     last_login: datetime = datetime.now()
@@ -27,14 +27,17 @@ class User(Document, BaseUser):
         ]
 
     @property
+    @override
     def is_authenticated(self) -> bool:
         return True
 
     @property
+    @override
     def display_name(self) -> str:
         return self.username
 
     @property
+    @override
     def identity(self) -> str:
         return self.username
 
@@ -59,7 +62,7 @@ class User(Document, BaseUser):
 
 class UserResponse(BaseModel):
     username: str
-    email: Optional[EmailStr]
+    email: EmailStr | None
     created_at: datetime
     last_login: datetime
     auth_token: uuid.UUID
@@ -68,9 +71,27 @@ class UserResponse(BaseModel):
 class CreateUser(BaseModel):
     username: str = Field(min_length=4, max_length=128)
     password: SecretStr = Field(min_length=8, max_length=128)
-    email: Optional[EmailStr] = None
+    email: EmailStr | None = None
 
 
 class AuthenticateUser(BaseModel):
     username: str = Field(min_length=4, max_length=128)
     password: SecretStr = Field(min_length=8, max_length=128)
+
+
+class UpdateUser(BaseModel):
+    username: str | None = Field(
+        default=None,
+        min_length=4,
+        max_length=128,
+    )
+    password: SecretStr | None = Field(
+        default=None,
+        min_length=8,
+        max_length=128,
+    )
+    email: EmailStr | None = Field(
+        default=None,
+        min_length=3,
+        max_length=128,
+    )
--- backend/overseer/routes/users.py
+++ backend/overseer/routes/users.py
@@ -3,7 +3,7 @@ from fastapi import status
 
 from starlette.authentication import requires
 
-from overseer.db.users import UserResponse
+from overseer.db.users import UserResponse, UpdateUser, User
 
 
 router = APIRouter(prefix='/users', tags=['users'])
@@ -13,5 +13,18 @@ router = APIRouter(prefix='/users', tags=['users'])
     '/profile', status_code=status.HTTP_200_OK, summary='Get user profile.'
 )
 @requires('authenticated')
-def profile(request: Request) -> UserResponse:
+async def profile(request: Request) -> UserResponse:
     return UserResponse.model_validate(request.user, from_attributes=True)
+
+
+@router.patch(
+    '/profile', status_code=status.HTTP_200_OK, summary='Update user profile.'
+)
+async def update_profile(request: Request, data: UpdateUser) -> UserResponse:
+    user: User = request.user
+    if data.password:
+        data.password = User.hash_password(data.password.get_secret_value())
+
+    user = user.model_copy(update=data.model_dump(exclude_unset=True))
+    await user.save()
+    return UserResponse.model_validate(user, from_attributes=True)
