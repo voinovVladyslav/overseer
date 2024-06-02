fb239e542d298edd7ef71175d344be6917d9c0ba
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-06-02 08:25:32 +0300

    feat: configurable keyword filter

--- backend/overseer/chronicler/filters.py
+++ backend/overseer/chronicler/filters.py
@@ -1,10 +1,26 @@
 from typing import override
 import logging
+from pydantic import BaseModel
 
 
-class NoPyMongoInfoFilter(logging.Filter):
+class FilterConfig(BaseModel):
+    keywords: list[str]
+    level: str
+
+    @property
+    def level_number(self) -> int:
+        return logging.getLevelName(self.level.upper())
+
+
+class KeywordLevelFilter(logging.Filter):
+    def __init__(self, filters: list[dict[str, str | list[str]]]) -> None:
+        self.filters = [FilterConfig(**f) for f in filters]
+        super().__init__()
+
     @override
     def filter(self, record: logging.LogRecord) -> bool | logging.LogRecord:
-        if 'pymongo' not in record.name:
-            return True
-        return record.levelno >= logging.WARNING
+        for filter in self.filters:
+            for keyword in filter.keywords:
+                if record.name.startswith(keyword):
+                    return record.levelno >= filter.level_number
+        return True
