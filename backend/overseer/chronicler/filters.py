391127fac237982d897aa8a9836b52429b1c4439
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-06-02 08:35:56 +0300

    feat: colorful terminal output for logs

--- backend/overseer/chronicler/filters.py
+++ backend/overseer/chronicler/filters.py
@@ -1,6 +1,17 @@
 from typing import override
 import logging
+
 from pydantic import BaseModel
+
+
+class Colors:
+    LIGHT_BLUE = "\033[38;5;39m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    LIGHT_YELLOW = "\033[93m"
+    RED = "\033[31m"
+    ORANGE = "\033[38;5;208m"
+    RESET = "\033[0m"
 
 
 class FilterConfig(BaseModel):
@@ -24,3 +35,25 @@ class KeywordLevelFilter(logging.Filter):
                 if record.name.startswith(keyword):
                     return record.levelno >= filter.level_number
         return True
+
+
+class ColorFilter(logging.Filter):
+    @override
+    def filter(self, record: logging.LogRecord) -> bool | logging.LogRecord:
+        if record.levelno >= logging.CRITICAL:
+            record.levelname = f"{Colors.ORANGE}{record.levelname}{Colors.RESET}"  # noqa
+
+        elif record.levelno >= logging.ERROR:
+            record.levelname = f"{Colors.RED}{record.levelname}{Colors.RESET}"
+
+        elif record.levelno >= logging.WARNING:
+            record.levelname = f"{Colors.YELLOW}{record.levelname}{Colors.RESET}"  # noqa
+
+        elif record.levelno >= logging.INFO:
+            record.levelname = f"{Colors.GREEN}{record.levelname}{Colors.RESET}"
+
+        elif record.levelno >= logging.DEBUG:
+            record.levelname = f"{Colors.LIGHT_BLUE}{record.levelname}{Colors.RESET}"  # noqa
+
+        record.name = f"{Colors.LIGHT_YELLOW}{record.module}{Colors.RESET}"
+        return record
